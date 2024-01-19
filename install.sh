ffdd5a0d92eb763ef57b63f63f64a6b8fd953bea
Jacob <87302129+py-er@users.noreply.github.com>
2024-01-19 10:36:17 +0100

    Update for use as root user

--- install.sh
+++ install.sh
@@ -1,18 +1,28 @@
+cd ~
 wget "https://raw.githubusercontent.com/py-er/script-to-path/main/script-to-path" 2> /dev/null
 if ! [[ $(ls) == *"script-to-path"* ]]
 then
-echo "Script-to-path could not be installed. Check internet connection and directory permissions"
+echo "Script-to-path could not be installed. Check internet connection."
 exit 1
 fi
 chmod +x script-to-path
 ./script-to-path script-to-path
 rm script-to-path
+cd - > /dev/null
 user=$(whoami)
-if ! [[ $PATH == *"/home/$user/.scripts"* ]]
+if ! [[ $PATH == *"/$user/.scripts"* ]]
+then
+if ! [[ $user == "root" ]]
 then
 export run PATH=/home/$user/.scripts:$PATH
 cd ~
 echo "export PATH=/home/$user/.scripts:$PATH" >> .bashrc
 echo "export PATH=/home/$user/.scripts:$PATH" >> .zshrc
 cd - > /dev/null
+else
+cd ~
+echo "export PATH=/$user/.scripts:$PATH" >> .bashrc
+echo "export PATH=/$user/.scripts:$PATH" >> .zshrc
+cd - > /dev/null
+fi
 fi
