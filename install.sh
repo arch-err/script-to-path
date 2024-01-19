71cd293346c0ec11f8ad220c28378aa74afa9e4c
Jacob <87302129+py-er@users.noreply.github.com>
2024-01-19 08:51:05 +0100

    Installtion test

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget "https://raw.githubusercontent.com/py-er/script-to-path/main/script-to-path" 2> /dev/null || echo "Could not download to $(pwd)" && exit 1
+wget "https://raw.githubusercontent.com/py-er/script-to-path/main/script-to-path" 1> /dev/null || exit 1
 chmod +x script-to-path
 ./script-to-path script-to-path
 rm script-to-path
