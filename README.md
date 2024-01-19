7a1baa8b911d2ddf5b5ef783d58c8c7c0c4581c8
Jacob <87302129+py-er@users.noreply.github.com>
2024-01-19 08:39:42 +0100

    Fix broken installation method

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Adds a script to /use/local/scripts/ and adds it to path
  - Instalation
 ```bash
 wget -qO- https://raw.githubusercontent.com/py-er/script-to-path/main/install.sh | bash -s arg1 arg2
+bash <(curl -s https://raw.githubusercontent.com/py-er/script-to-path/main/install.sh)
 ```
  
--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
-wget "https://raw.githubusercontent.com/py-er/script-to-path/main/script-to-path" 2> /dev/null; chmod +x script-to-path; ./script-to-path script-to-path; rm script-to-path; user=$(whoami); if ! [[ $PATH == *"/home/$user/.scripts"* ]]; then; export run PATH=/home/$user/.scripts:$PATH; cd ~; echo "export PATH=/home/$user/.scripts:$PATH" >> .bashrc; echo "export PATH=/home/$user/.scripts:$PATH" >> .zshrc; cd -; fi;
+wget "https://raw.githubusercontent.com/py-er/script-to-path/main/script-to-path" 2> /dev/null
+chmod +x script-to-path
+./script-to-path script-to-path
+rm script-to-path
+user=$(whoami)
+if ! [[ $PATH == *"/home/$user/.scripts"* ]]
+then
+export run PATH=/home/$user/.scripts:$PATH
+cd ~
+echo "export PATH=/home/$user/.scripts:$PATH" >> .bashrc
+echo "export PATH=/home/$user/.scripts:$PATH" >> .zshrc
+cd -;
+fi
